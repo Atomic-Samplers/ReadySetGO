8a97c2222a48173f1a7273c4f7ff815f25b8449d
Julian <holland@fhi.mpg.de>
2025-09-02 07:50:09 +0200

    added gaussian, for real

---
+++ src/readysetgo/structure_clustering/clustering_algorithms/hashing_clustering_gauss_algorithm.py
@@ -0,0 +1,8 @@
+from readysetgo.structure_clustering.clustering_algorithms.hashing_clustering_algorithm import (
+    HashingClusteringAlgorithm
+)
+import numpy as np
+class HashingClusteringGaussAlgorithm(HashingClusteringAlgorithm):
+    
+    def get_normalisation_array(self):
+        return np.random.normal(1, self.tolerance * 0.5, size=self.normalizations)
